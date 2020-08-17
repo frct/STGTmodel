be5d92d99b78f30c57e45332cd09b729b3ce235a
frct <frct@users.noreply.github.com>
2020-08-17 17:05:02 +0100

    Create ExtractBestParameters.py
    Best parameters extraction

---
+++ Standard model/ExtractBestParameters.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 17 16:33:07 2020
+
+@author: franc
+"""
+
+import numpy as np
+
+n_rats = 20
+n_parameters = 5
+
+best_params = np.zeros([n_rats, n_parameters + 1])
+
+for rat in range(n_rats):
+    params = np.load('Rat' + str(rat+1) + ' parameters.npy')
+    negLL = np.load('Rat' + str(rat+1) + ' negLL.npy')
+    best_idx = np.argmin(negLL)
+    
+    best_params[rat,:] = np.append(params[best_idx,:], negLL[best_idx])
